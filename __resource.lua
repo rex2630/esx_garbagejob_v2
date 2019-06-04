0d65656ba40be284b240187a8c91ddf6fae3385a
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-06-04 19:13:25 +0200

    Included
    de.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,7 @@
 server_scripts {
     '@es_extended/locale.lua',
 	'locales/en.lua',
+	'locales/de.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -8,6 +9,7 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
+	'locales/de.lua',
 	'config.lua',
 	'client/main.lua'
 }
